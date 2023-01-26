98e9d8a175996b1202fbec4376718e3487053451
CDaws614 <connor.dawson01@icloud.com>
2023-01-25 22:12:23 -0600

    Initial testing
    Pulled repo, added file on new branch, all seems well so far...

---
+++ Assets/Scripts/GitHub_Test.cs
@@ -0,0 +1,27 @@
+/*
+ * This is a dummy file
+ * I am just testing GitHub
+ * Feel free to delete me later
+ * -Connor
+ */
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitHub_Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }   
+}
